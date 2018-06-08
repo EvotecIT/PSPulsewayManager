f76a36fdff59fedaf27483c468e1e5c5719cde8b
PrzemyslawKlys <pklys@evotec.pl>
2018-06-08 17:59:54 +0200

    Speed up of registry functions

--- Private/SupportFunctions.ps1
+++ Private/SupportFunctions.ps1
@@ -53,27 +53,71 @@ function Set-RegistryRemotly {
 function Set-RegistryRemote {
     [cmdletbinding()]
     param(
-        $Computer,
-        $RegistryPath,
+        [string[]]$Computer,
+        [string] $RegistryPath,
         [string[]]$RegistryKey,
-        [object[]]$Value
+        [string[]]$Value
     )
+
+    $ScriptBlock = {
+        #[cmdletbinding()]
+        param(
+            [string] $RegistryPath,
+            [string[]] $RegistryKey,
+            [string[]] $Value
+        )
+        $VerbosePreference = $Using:VerbosePreference
+        $List = New-Object System.Collections.ArrayList
+
+        for ($i = 0; $i -lt $RegistryKey.Count; $i++) {
+
+            Write-Verbose "REG WRITE: $RegistryPath REGKEY: $($RegistryKey[$i]) REGVALUE: $($Value[$i])"
+            $Setting = Set-ItemProperty -Path $RegistryPath -Name $RegistryKey[$i] -Value $Value[$i] #-PassThru
+            $List.Add($Setting) | Out-Null
+        }
+        return $List
+    }
+
+    $ListComputers = New-Object System.Collections.ArrayList
+    foreach ($Comp in $Computer) {
+        $Return = Invoke-Command -ComputerName $Computer -ScriptBlock $ScriptBlock -ArgumentList $RegistryPath, $RegistryKey, $Value
+        $ListComputers.Add($Return) | Out-Null
+    }
+    return $ListComputers
+}
+function Get-RegistryRemote {
+    [cmdletbinding()]
+    param(
+        [string[]]$Computer = $Env:COMPUTERNAME,
+        [string]$RegistryPath,
+        [string[]]$RegistryKey
+    )
+
 
     $ScriptBlock = {
         [cmdletbinding()]
         param(
-            $RegistryPath,
-            $RegistryKey,
-            $Value
+            [string]$RegistryPath,
+            [string[]]$RegistryKey
         )
         $VerbosePreference = $Using:VerbosePreference
-        $Setting = Set-ItemProperty -Path $RegistryPath -Name $RegistryKey -Value $Value #-PassThru
-        return $Setting
+        $List = New-Object System.Collections.ArrayList
+
+        #Write-Verbose "REG READ: $RegistryPath REGKEY: $($RegistryKey)"
+
+        for ($i = 0; $i -lt $RegistryKey.Count; $i++) {
+            $RegKey = $RegistryKey[$i]
+            Write-Verbose "REG READ: $RegistryPath REGKEY: $RegKey"
+            $Setting = Get-ItemProperty -Path $RegistryPath -Name $RegKey
+            Write-Verbose "REG VALUE: $($Setting.$RegKey)"
+            $List.Add($Setting.$RegKey) | Out-Null
+        }
+        return $List
     }
-    $List = New-Object System.Collections.ArrayList
+    $ListComputers = New-Object System.Collections.ArrayList
     foreach ($Comp in $Computer) {
-        $Return = Invoke-Command -ComputerName $Comp -ScriptBlock $ScriptBlock -ArgumentList $RegistryPath, $RegistryKey, $Value
-        $List.Add($Return) | Out-Null
+        $Return = Invoke-Command -ComputerName $Comp -ScriptBlock $ScriptBlock -ArgumentList $RegistryPath, $RegistryKey
+        $ListComputers.Add($Return) | Out-Null
     }
-    return $List
+    return $ListComputers
 }
