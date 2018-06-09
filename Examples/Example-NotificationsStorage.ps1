656fe99f4dc32f9d7ea4a8c23169b73707e1e1a9
PrzemyslawKlys <pklys@evotec.pl>
2018-06-09 13:50:51 +0200

    Example

--- Examples/Example-NotificationsStorage.ps1
+++ Examples/Example-NotificationsStorage.ps1
@@ -2,6 +2,23 @@ Clear-Host
 Import-Module PSPulsewayManager -Force
 Import-Module PSWriteColor
 
-$Drives = Get-PulsewayLocalDiskSpace -Verbose
+$Computer = 'AD1'
+
+Write-Color 'Get ', 'Low Disk Space' -Color White, Yellow
+$Drives = Get-PulsewayLocalDiskSpace -Computer $Computer
 $Drives
+Write-Color 'List ', ' drives separatly' -Color White, Yellow -LinesAfter 1
 $Drives.MonitoredDrives
+
+## Find all drives for computer/server
+$FindDrives = Get-Drive -Computer $Computer
+$ListDrives = @()
+# Set the settings for all drives for particular computer/server (Elevated)
+$DrivesElevated = Set-DriveSettings -Drive $FindDrives -Percentage 10 -Priority Elevated -SizeMB 20000 -UsePercentage No
+# Repeat process for same drives but make it critical
+$DrivesCritical = Set-DriveSettings -Drive $FindDrives -Percentage 10 -Priority Critical -SizeMB 10000 -UsePercentage No
+
+$ListDrives += $DrivesElevated
+$ListDrives += $DrivesCritical
+
+Set-PulsewayLocalDiskSpace -Computer $Computer -Drives $ListDrives -SendNotificationOnLowHDDSpace Enabled -Verbose
